123efba6a5c057f716b935e21a830677fde3a481
danosaur98 <daniel.zhou2012@gmail.com>
2018-01-21 01:20:40 -0500

    pre-refactor

--- app/src/main/java/com/example/danielzhou/pennapps/MainActivity.java
+++ app/src/main/java/com/example/danielzhou/pennapps/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.danielzhou.pennapps;
 
 import android.content.Intent;
-import android.support.v7.widget.Toolbar;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,10 +13,8 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
 import org.json.JSONArray;
@@ -29,18 +23,18 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextView;
     private ListView mListView;
-    private CustomListAdapter whatever;
+    private Button mButton;
+    private CustomListAdapter mCustomListAdapter;
     final ArrayList<String> nameArray = new ArrayList<>();
     final ArrayList<String> amountArray = new ArrayList<>();
     final ArrayList<String> charityArray = new ArrayList<>();
     final ArrayList<String> endDateArray = new ArrayList<>();
     final ArrayList<String> lotteryIDArray = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         AsyncTask.execute(new Runnable() {
@@ -83,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
-                                whatever.notifyDataSetChanged();
+                                mCustomListAdapter.notifyDataSetChanged();
 
                             }
                         }
@@ -94,23 +88,23 @@ public class MainActivity extends AppCompatActivity {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-            }});
+            }
+        });
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = findViewById(R.id.textView);
-        whatever = new CustomListAdapter(this, nameArray, amountArray, charityArray, endDateArray);
+        mCustomListAdapter = new CustomListAdapter(this, nameArray, amountArray, charityArray, endDateArray);
         mListView = (ListView) findViewById(R.id.listviewID);
-        mListView.setAdapter(whatever);
+        mListView.setAdapter(mCustomListAdapter);
         Button newLottery = (Button) findViewById(R.id.button);
         newLottery.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // TODO Auto-generated method stub
-                    Intent i = new Intent(getApplicationContext(),CreateNewLottery.class);
-                    startActivity(i);
-                }
-            });
-        whatever.notifyDataSetChanged();
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getApplicationContext(), CreateNewLottery.class);
+                startActivity(i);
+            }
+        });
+        mCustomListAdapter.notifyDataSetChanged();
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position,
@@ -124,13 +118,79 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        mButton = findViewById(R.id.refresh);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AsyncTask.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        nameArray.clear();
+                        amountArray.clear();
+                        charityArray.clear();
+                        endDateArray.clear();
+                        lotteryIDArray.clear();
+                        OkHttpClient client = new OkHttpClient();
+
+                        Request request = new Request.Builder()
+                                .url("https://whispering-scrubland-39491.herokuapp.com/getLotteries")
+                                .get()
+                                .addHeader("cache-control", "no-cache")
+                                .addHeader("postman-token", "b866a099-64e6-4978-07e7-ce2304ab4b0d")
+                                .build();
+                        try {
+                            Response response = client.newCall(request).execute();
+                            String jsonData = response.body().string();
+                            JSONObject Jobject = null;
+                            try {
+                                Jobject = new JSONObject(jsonData);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            final JSONArray Jarray = Jobject.getJSONArray("result");
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    for (int i = 0; i < Jarray.length(); i++) {
+                                        JSONObject lottery = null;
+                                        try {
+                                            lottery = Jarray.getJSONObject(i);
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                        try {
+                                            nameArray.add(lottery.getString("title"));
+                                            amountArray.add(lottery.getString("total"));
+                                            charityArray.add(lottery.getString("charity"));
+                                            endDateArray.add(lottery.getString("endDate"));
+                                            lotteryIDArray.add(lottery.getString("lotteryID"));
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                        mCustomListAdapter.notifyDataSetChanged();
+
+                                    }
+                                }
+                            });
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
     }
+
     @Override
     protected void onResume() {
         super.onResume();
-        whatever.notifyDataSetChanged();
+        mCustomListAdapter.notifyDataSetChanged();
 
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
